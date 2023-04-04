18ebd55e9b427db3e11b06bcd7122dd02f81347e
Williamatthewood <w.wood@tcu.edu>
2023-04-03 19:24:58 -0500

    citation on README, working restaurant map function, and responsive map design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,23 @@
-var userZipCode = 75052;
-var chosenCuisine = "mexican";
+var userZipCode;
+var chosenFoodGenre;
+
 var restaurantMap = document.getElementById("restaurant-map");
 var zipCodeText = document.getElementById("zipcode");
-var cuisineText = document.getElementById("cuisine");
+var cuisineText = document.getElementById("foodGenre");
 
-//on click event
+//on click/ submit event
 function generateRestaurantMap(){
     //if on a submit event, stop the page from reloading
     // event.PreventDefault();
+
     //take user zipcode and cuisine selections
     // userZipCode = zipCodeText.value;
     // chosenCuisine = cuisineText.value;
+
     //generate the API link
     var mapLink = 
     "https://www.google.com/maps/embed/v1/search?key=AIzaSyCH6SpD7Rqx4pD0Y7ZsOK8h1wkkIbV8Ptg&q=" 
-    + chosenCuisine + "+restaurants+near+" + userZipCode;
+    + chosenFoodGenre + "+restaurants+near+" + userZipCode;
     //add the link to the src attribute on the HTML page
     restaurantMap.setAttribute("src", mapLink);
 
