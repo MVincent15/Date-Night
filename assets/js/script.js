d3e4fe88380f72cc08d9dcee73a8c547b8d4f52b
Williamatthewood <w.wood@tcu.edu>
2023-04-03 21:07:42 -0500

    user preferences stored in local storage, and google map updates based on zipcode and cuisine

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,12 @@ var movieGenreText = document.getElementById("movieGenre");
 var submitButton = document.getElementById("submit");
 
 function init(){
+    //pull user preferences from local storage
     var storedMovieGenre = JSON.parse(localStorage.getItem("movieGenre"));
     var storedFoodGenre = JSON.parse(localStorage.getItem("foodGenre"));
     var storedZipCode = JSON.parse(localStorage.getItem("zipcode"));
 
+    //check if there is data stored in each, if there is, it updates the input field to the saved value
     if (storedMovieGenre !== null){
         movieGenreText.value = storedMovieGenre;
     }
@@ -24,16 +26,15 @@ function init(){
     if (storedZipCode !== null){
         zipCodeText.value = storedZipCode;
     }
+
+    //updates the restaurant map with previous selections on load
+    generateRestaurantMap();
 }
 
 
-//on click/ submit event
 function generateRestaurantMap(){
-    //if on a submit event, stop the page from reloading
-    // event.PreventDefault();
 
     //take user zipcode and cuisine selections
-
     userZipCode = zipCodeText.value;
     chosenFoodGenre = foodGenreText.value;
 
@@ -47,10 +48,17 @@ function generateRestaurantMap(){
 }
 
 function storePreferences(event){
+    //prevent page from reloading
     event.preventDefault();
+    //store each field into it's own slot in local storage
     localStorage.setItem("movieGenre", JSON.stringify(movieGenreText.value));
     localStorage.setItem("foodGenre", JSON.stringify(foodGenreText.value));
     localStorage.setItem("zipcode", JSON.stringify(zipCodeText.value));
+    //generate the restaurant map
+    generateRestaurantMap();
 }
 
-submitButton.addEventListener("submit", storePreferences);
+submitButton.addEventListener("click", storePreferences);
+
+//run on load
+init();
